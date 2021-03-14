c1df880d6dfa4d16893857c9e80c207cae577245
HR Williams <halrubinwilliams@gmail.com>
2021-03-13 22:26:45 -0800

    Update mars constructor test for good fail with nextPhaseAge key value

--- __tests__/mars.test.js
+++ __tests__/mars.test.js
@@ -3,8 +3,9 @@ import Mars from './../src/mars.js';
 describe('Mars', () => {
 
   test("should correctly create a mars object with a user's earth age", () => {
-    const exampleMars = new Mars(40);
+    const exampleMars = new Mars(40,99);
     expect(exampleMars.earthAge).toEqual(40);
+    expect(exampleMars.nextPhaseAge).toEqual(99)
   });
 
   test ("should correctly determine a person's age in Mars years", () => {
