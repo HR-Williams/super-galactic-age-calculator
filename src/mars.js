629281dfc9061553e7dafc0a987e08b2f3b4619c
HR Williams <halrubinwilliams@gmail.com>
2021-03-13 22:28:54 -0800

    Update Mars constructor to include nextPhaseAge keyvalue and pass test

--- src/mars.js
+++ src/mars.js
@@ -1,6 +1,7 @@
 export default class Mars {
-  constructor(earthAge) {
+  constructor(earthAge, nextPhaseAge) {
     this.earthAge = earthAge; 
+    this.nextPhaseAge = nextPhaseAge;
   }
 
   marsAge() {
