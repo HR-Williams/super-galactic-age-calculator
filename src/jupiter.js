fb950f7e94765f57ce0e03cc832d05d746639577
HR Williams <halrubinwilliams@gmail.com>
2021-04-06 11:41:56 -0700

    s

--- src/jupiter.js
+++ src/jupiter.js
@@ -18,6 +18,11 @@ export default class Jupiter {
 
   stringifyTime() {
     let userEtherTime = (this.enterEther() - this.jupiterAge());
-    return (`You are expected to have ${userEtherTime} more Jupiter years until you will leave your body to enter the ether.`)
+    let userBonusTime =(this.jupiterAge() - this.enterEther());
+    if (this.earthAge < this.nextPhaseAge) {
+      return (`You are expected to have ${userEtherTime} more Jupiter years until you will leave your body to enter the ether.`)
+    } else {
+      return  (`You have accrued ${userBonusTime} Jupiter years of Bonus time.`)
+    }
   }
 }
