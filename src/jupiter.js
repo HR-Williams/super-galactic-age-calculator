99d8a4f4aad8bb61768f6b0977350d5a116fd336
HR Williams <halrubinwilliams@gmail.com>
2021-04-06 11:33:40 -0700

    Add code block to pass stringify time for jupiter

--- src/jupiter.js
+++ src/jupiter.js
@@ -17,6 +17,7 @@ export default class Jupiter {
   }
 
   stringifyTime() {
-  return "string"
+    let userEtherTime = (this.enterEther() - this.jupiterAge());
+    return (`You are expected to have ${userEtherTime} more Jupiter years until you will leave your body to enter the ether.`)
   }
 }
